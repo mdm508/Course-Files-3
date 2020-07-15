623af055b4fe0fa0e4a07e7de1e1282a6622c008
Kern  Mittal <tcsirvkmittal@gmail.com>
2020-07-15 00:44:06 +0000

    Initial commit

---
+++ 1-Course files/chapter02/convert 5 times.py
@@ -0,0 +1,11 @@
+# convert.py
+#     A program to convert Celsius temps to Fahrenheit
+# by: Susan Computewell
+
+def main():
+    for i in range(5):
+        celsius = eval(input("What is the Celsius temperature? "))
+        fahrenheit = 9/5 * celsius + 32
+        print("The temperature is", fahrenheit, "degrees Fahrenheit.")
+
+main()
